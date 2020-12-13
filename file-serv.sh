8631437bdc0e5c18e963d9f224360def453b52f9
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2020-12-14 01:28:36 +0200

    Update file-serv.sh

--- file-serv.sh
+++ file-serv.sh
@@ -12,13 +12,5 @@ sudo mv site1/index.html /var/www/site1
 sudo mv site1/style.css /var/www/site1
 sudo mv site1/virobnik.html /var/www/site1
 sudo mv site1/zalslav.html /var/www/site1 
-#sudo nano /var/www/site1/index.html	#макет html
-sudo mv site1.conf /etc/apache2/sites-available/site1.conf 	#копіювати вміст 000df
-#<VirtualHost *:80>
-#ServerName www.example.com
-#ServerAdmin webmaster@localhost
-#DocumentRoot /var/www/site1
-#ErrorLog ${APACHE_LOG_DIR}/error.log
-#CustomLog ${APACHE_LOG_DIR}/access.log combined
-#</VirtualHost>
+
 
