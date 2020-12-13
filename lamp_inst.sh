d2153c273e6c21fe06441069702f382f299fd1a2
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2020-12-13 19:44:53 +0200

    Update lamp_inst.sh

--- lamp_inst.sh
+++ lamp_inst.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-sudo bash deploy.sh
 sudo bash apach-inst.sh
 sudo bash apach-actv.sh
 sudo bash file-serv.sh
 sudo bash mySQL-inst.sh
 sudo bash mySQL-set.sh
 sudo bash php-inst.sh
-
+sudo bash deploy.sh
 
