e53665dad42e82626bd17ddde8a8e4e1dafd54ed
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2020-12-13 19:43:54 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,12 @@ echo Now mount the site
 sleep 5
 git clone https://github.com/Lucky-graf/site1.git
 sudo mkdir /var/www/
-sudo mv site1 /var/www/
+sudo mv site1/images1 /var/www/site1
+sudo mv site1/car.html /var/www/site1
+sudo mv site1/car2.png /var/www/site1
+sudo mv site1/honki.html /var/www/site1
+sudo mv site1/honshchik.html /var/www/site1
+sudo mv site1/index.html /var/www/site1
+sudo mv site1/style.css /var/www/site1
+sudo mv site1/virobnik.html /var/www/site1
+sudo mv site1/zalslav.html /var/www/site1 
