679eec7166c8088f031a98d378ecf049356b0524
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2020-12-14 01:30:12 +0200

    Update apach-actv.sh

--- apach-actv.sh
+++ apach-actv.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-
+sudo mv site1.conf /etc/apache2/sites-available/site1.conf 	#копіювати вміст 000df
+#<VirtualHost *:80>
+#ServerName www.example.com
+#ServerAdmin webmaster@localhost
+#DocumentRoot /var/www/site1
+#ErrorLog ${APACHE_LOG_DIR}/error.log
+#CustomLog ${APACHE_LOG_DIR}/access.log combined
+#</VirtualHost>
 sudo a2ensite site1	# your_domain.conf
 sudo a2dissite 000-default.conf
 sudo apache2ctl configtest
