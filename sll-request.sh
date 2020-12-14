ff67a184de1d16c310697e214409349e285fdc34
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2020-12-14 03:12:01 +0200

    Update sll-request.sh

--- sll-request.sh
+++ sll-request.sh
@@ -1,16 +1,14 @@
-  GNU nano 2.9.3                                        test2.sh                                                   
 #!/bin/bash
-echo -n "Продолжить? (y/n) "
+echo -n 'First create a domain to create an HTTPS certificate' \n 'Done? (y/n)'
 read item
 case "$item" in
-    y|Y) echo "Ввели «y», продолжаем..."
+    y|Y) echo "Еntered «y», continue..."
         sudo bash cerbot.sh
         ;;
-    n|N) echo "Ввели «n», завершаем..."
-        sudo bash sll-request.sh
+    n|N) echo "Еntered «n», end..."
         exit 0
         ;;
-    *) echo "Ничего не ввели. Выполняем действие по умолчанию..."
+    *) echo "Nothing was entered. Performing the default action..."
         sudo bash sll-request.sh
         exit 0
         ;;
