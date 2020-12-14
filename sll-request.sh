28e24e548b1c8309b2de3275f8bd598ed00c80aa
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2020-12-14 03:00:38 +0200

    Update and rename cerbot.sh to sll-request.sh

--- sll-request.sh
+++ sll-request.sh
@@ -1,12 +1,17 @@
+  GNU nano 2.9.3                                        test2.sh                                                   
+#!/bin/bash
 echo -n "Продолжить? (y/n) "
-
 read item
 case "$item" in
     y|Y) echo "Ввели «y», продолжаем..."
+        sudo bash cerbot.sh
         ;;
     n|N) echo "Ввели «n», завершаем..."
+        sudo bash sll-request.sh
         exit 0
         ;;
     *) echo "Ничего не ввели. Выполняем действие по умолчанию..."
+        sudo bash sll-request.sh
+        exit 0
         ;;
 esac
