8a4d0834229a5736f96855866bc8b3dc9ffd36e3
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2020-12-14 04:00:58 +0200

    Update cerbot.sh

--- cerbot.sh
+++ cerbot.sh
@@ -2,3 +2,4 @@ sudo snap install core; sudo snap refresh core
 sudo snap install --classic certbot
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
 sudo certbot --apache
+sudo certbot certonly --dry-run -d site-car.ga -d site-car.ga
